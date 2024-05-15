d9077727e82da07a75275fa8e8b8dd6e10ef2ff2
Rajbir Ahmed <160039377+Rajbirsakib@users.noreply.github.com>
2024-05-16 00:22:10 +0600

    Add files via upload

---
+++ 4A - Watermelon.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int w;
+    cin>>w;
+    if(w%2==0 && w>2 && w>=1 && w<=100) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}

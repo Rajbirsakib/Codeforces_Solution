f94fe848b960b4893ab86c4e2560f830f99fa049
Rajbir Ahmed <160039377+Rajbirsakib@users.noreply.github.com>
2025-01-01 17:46:03 +0600

    Add files via upload

---
+++ 993A - Easy Problem.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<n-1<<endl;
+    }
+
+    return 0;
+}

d03d206fc7837aafc5870be81f5b5f7da82c8d3b
Rajbir Ahmed <160039377+Rajbirsakib@users.noreply.github.com>
2025-04-13 22:47:25 +0600

    Add files via upload

---
+++ 1017A - Trippi Troppi.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        string s1,s2,s3;
+        cin>>s1>>s2>>s3;
+        char a= s1[0], b=s2[0], c=s3[0];
+        cout<<a<<b<<c;
+        cout<<endl;
+    }
+
+    return 0;
+}
